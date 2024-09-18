c342877d240af74683dd9aed15d5f5494d086fb7
SmithsonArrey <66339786+SmithsonArrey@users.noreply.github.com>
2024-09-18 11:59:30 -0500

    added config

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 )
 
 func main() {
-	app := application.New()
+	app := application.New(application.LoadConfig())
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
-	
+
 	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
