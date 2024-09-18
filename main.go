ffec7bc564ffc80fd670d571e7ec43ce16e1c3f1
SmithsonArrey <66339786+SmithsonArrey@users.noreply.github.com>
2024-09-18 02:28:34 -0500

    added order handlers

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-)
 	
+	"github.com/dreamsofcode-io/orders-api/application"
+)
 
 func main() {
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
-	}
+	app := application.New()
 
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
 
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Hello, world!"))
 }
